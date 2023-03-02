a0a795434f0c216037bb10962104177fd80c8478
E.A.T.I <eati@labs.cs.uns.edu.ar>
2023-03-02 16:04:47 -0300

    Inicio

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+"""EATI2023 Rodrigo Kraus"""
+
+import os.path
+
+
+def main():
+    """Ejecuto el método main."""
+
+    if os.path.exists('build'):
+        print('Ya existe el archivo')
+    else:
+        os.mkdir('build')
+        with open(r'build\index.html', 'x',  encoding="utf8"):
+            print('Archivo abierto exitosamente.')
