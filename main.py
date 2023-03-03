7530afa7d6fc015e0a3525225fdc60d227e8a50b
Rodri Kraus <rodrikraus10@hotmail.com>
2023-03-02 23:48:22 -0300

    asdsa

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-"""EATI2023 Rodrigo Kraus"""
+#!/usr/bin/python3
 
+"""
+Esto es parte de la actividad sobre pipeline del dia jueves
+"""
 import os.path
 
 
 def main():
-    """Ejecuto el método main."""
+    """
+    Si no existe, crea la carpeta build y el archivo index.html
+    """
+    folder = 'build/'
+    file = folder + 'index.html'
 
-    if os.path.exists('build'):
-        print('Ya existe el archivo')
+    if os.path.exists(folder):
+        print('\nNo se pudo crear la carpeta nueva, ya existia')
     else:
-        os.mkdir('build')
-        with open(r'build\index.html', 'x',  encoding="utf8"):
-            print('Archivo abierto exitosamente.')
+        os.mkdir(folder)
+        print('\nCarpeta creada exitosamente.')
+
+    if os.path.isfile(file):
+        print('No se pudo crear el archivo nuevo, ya existia\n')
+    else:
+        with open(file, 'w', encoding="utf-8") as my_file:
+            my_file.write('Este archivo fue creado con Python.\n')
+            print("Archivo creado exitosamente")
 
 
-main()
+if __name__ == '__main__':
+    main()
+
