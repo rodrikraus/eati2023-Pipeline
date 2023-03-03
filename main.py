4f12bb66e6be1bcaf14a3f3f0068ddffa36e9179
Rodri Kraus <rodrikraus10@hotmail.com>
2023-03-03 00:25:50 -0300

    casifinal

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ import os.path
 
 
 def main():
-    """
-    Si no existe, crea la carpeta build y el archivo index.html
-    """
+    """Se ejecuta el método main"""
 
-    if os.path.exists('build/'):
+    if os.path.exists('build'):
         print('\nLa carpeta build ya existe.')
     else:
-        os.mkdir('build/')
+        os.mkdir('build')
         print('\nSe creó la carpeta build.')
 
     if os.path.isfile('build/index.html'):
         print('El archivo index ya existe en la carpeta build\n')
     else:
         with open('build/index.html', 'w', encoding="utf-8"):
-            print("Archivo creado exitosamente")
+            print("index.html creado exitosamente.")
 
 
 if __name__ == '__main__':
